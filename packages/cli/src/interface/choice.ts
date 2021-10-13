6b8301d4b90989e2c642b830ae276f44b83148ee
pankace <44595774+pankace@users.noreply.github.com>
2021-10-14 02:13:27 +0700

    Create choice.ts

---
+++ packages/cli/src/interface/choice.ts
@@ -0,0 +1,44 @@
+export interface Answer {
+    gitprov: Provivder;
+    gituser: string;
+    site_type: sitetype;
+    files: Object;
+    overwrite?: boolean;
+}
+export interface Choice {
+    name: string;
+    value: UniversalChoiceValue | 
+           github | 
+           sitetype |
+           Provivder;
+}
+
+export enum UniversalChoiceValue {
+    ALL = 'ALL',
+    LICENSE = 'LICENSE',
+    README = 'README',
+    PACKAGE_JSON = 'package.json',
+    TSCONFIG = 'tsconfig.json',
+    YARN_LOCK = 'yarn.lock',
+    NEXT_ENV = 'next-env.d.ts',
+    NEXT_CONFIG = 'next.config.js',
+    POSTCSS_CONFIG = 'postcss.config.js',
+    TAILWIND_CONFIG = 'tailwind.config.js',
+    CONTRIBUTING = 'CONTRIBUTING.md',
+
+}
+
+export enum github {
+    BUG_REPORT = 'BUG_REPORT',
+    FEATURE_REQUEST = 'FEATURE_REQUEST',
+    PULL_REQUEST = 'PULL_REQUEST',
+    SECURITY = 'SECURITY',
+}
+
+export enum sitetype {
+    NEXTJS_STATIC = ' NEXTJS_STATIC',
+    NEXTJS = 'NEXTJS'
+}
+export enum Provivder {
+    GITHUB = 'Github'
+}

92cb5672a1df088fc8f9ad0feee7a3df0ad6e940
pankace <44595774+pankace@users.noreply.github.com>
2021-10-14 02:18:27 +0700

    updated interfaces :space_invader:

---
+++ packages/cli/src/interface/terminal-ptint.ts
@@ -0,0 +1,11 @@
+export enum ConsoleMessage {
+    Title = 'Block',
+    Subtitle = 'Defult website genrator for brikl',
+    Error = 'Fail: ',
+    Success = 'Success: ',
+    Information = 'More_Information: ',
+    Genarated = 'Genarated: ',
+    Generating = 'this is a generator which creates electricity through jokes that were confusing but funny regardless.',
+    Update = "Updated"
+}
+ 

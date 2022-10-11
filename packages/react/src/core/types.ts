922260c52906d5d8fadfcdac67272bd2e67c1cd6
AndrewPMurray <andrew@brikl.com>
2022-10-11 10:01:31 -0400

    fix(storefront): fix implicit children type error in react18

--- packages/react/src/core/types.ts
+++ packages/react/src/core/types.ts
@@ -1,9 +1,11 @@
 import type { FunctionComponent } from 'react'
 
-import type { ContextInitialize, CognitoConfig } from '@brikl/blocks'
+import type { ContextInitialize } from '@brikl/blocks'
+import React from 'react'
 
 export interface StorefrontProviderProps {
   config: ContextInitialize
+  children?: React.ReactElement
 }
 
 export type StorefrontProviderComponent =
